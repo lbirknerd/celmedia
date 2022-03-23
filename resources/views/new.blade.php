015bc1def37fc7dee415a363e79e89c3a93f937a
Lukas Birkner Davila <lbirkner@ENGGMT-0001.Celmedia.chile>
2022-03-23 09:20:44 -0300

    second commit

---
+++ resources/views/new.blade.php
@@ -0,0 +1,289 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{url('css/style.css')}}">
+</head>
+    
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <img src="img/coop.png" alt="err"/> 
+    <div class="head row container short">
+        <p>"Nombre var"</p>
+        <p>COINCO Nro 1004 ALERCE SUR</p>
+        <p>PUERTO MONTT-REGION DE LOS LAGOS</p>
+        <br>
+        <br>
+        <br>
+    </div>
+    <h3 class="title">Comunicación Trimestral Crédito de Consumo</h3>
+    <div class="container">
+    
+        <table class="table table-bordered w-auto">
+            <tbody>
+                <tr>
+                    <th>Nombre Titular</th>
+                    <th>9000</th>
+                </tr>
+                <tr>
+                    <th>Fecha de la Información</th>
+                    <th>x</th>
+                </tr>
+                <tr>
+                    <th>ID Producto</th>
+                    <th>x</th>
+                </tr>
+            </tbody>
+        </table>
+    
+    </div>
+    <div class="container justify-content-start">
+        <h3 class="subt">I. Producto Principal</h3>
+    </div>
+    
+    <div class="container product col-12">
+        <hr>
+        <div class="caev container col-2">
+                CAEV: <br> 18.00%
+        </div>
+        <table class="table table-bordered test">
+            <tbody>
+                <tr>
+                    <th>Plazo de Crédito:</th>
+                    <th>9000</th>
+                </tr>
+                <tr>
+                    <th>Saldo del Crédito:</th>
+                    <th>x</th>
+                </tr>
+                <tr>
+                    <th>Valor de la cuota (pesos):</th>
+                    <th>x</th>
+                </tr>
+                <tr>
+                    <th>Fecha Próximo Pago:</th>
+                    <th>x</th>
+                </tr>
+                <tr>
+                    <th>Costo Total de Prepago:</th>
+                    <th>x</th>
+                </tr>
+                <tr>
+                    <th>Garantias Vigentes:</th>
+                    <th>x</th>
+                </tr>
+                <tr>
+                    <th>Número de Cuota:</th>
+                    <th>x</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+    <div class="container justify-content-start">
+        <h3 class="subt">II. Historial</h3>
+    </div>
+    
+    <div class="container">
+        <hr>
+        <table class="table table-bordered w-auto ">
+            <tbody>
+                <tr>
+                    <th>Número de Cuotas Pagadas:</th>
+                    <th>9000</th>
+                </tr>
+                <tr>
+                    <th>Número de Cuotas Vencidas no Pagadas:</th>
+                    <th>x</th>
+                </tr>
+                <tr>
+                    <th>Monto Vencido no Pagado:</th>
+                    <th>x</th>
+                </tr>
+            </tbody>
+        </table>
+        
+        <div class="row">
+            <div class="col-12">
+                <p>Detalle</p>
+            </div>          
+            <div>
+            <table class="table table-bordered w-auto">
+                <tbody>
+                    <tr>
+                        <th>Cuota N°</th>
+                        <th>Monto Pagado</th>
+                        <th>Fecha vencimiento</th>
+                    </tr>
+                    <tr>
+                        <th>x</th>
+                        <th>x</th>
+                        <th>x</th>
+                    </tr>
+                    <tr>
+                        <th>x</th>
+                        <th>x</th>
+                        <th>x</th>
+                    </tr>
+                    <tr>
+                        <th>x</th>
+                        <th>x</th>
+                        <th>x</th>
+                    </tr>
+                    <tr>
+                        <th>x</th>
+                        <th>x</th>
+                        <th>x</th>
+                    </tr>
+                    <tr>
+                        <th>x</th>
+                        <th>x</th>
+                        <th>x</th>
+                    </tr>
+                    <tr>
+                        <th>x</th>
+                        <th>x</th>
+                        <th>x</th>
+                    </tr>
+                    <tr>
+                        <th>x</th>
+                        <th>x</th>
+                        <th>x</th>
+                    </tr>
+                    <tr>
+                        <th>x</th>
+                        <th>x</th>
+                        <th>x</th>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+        </div>
+    </div>
+    <div class="container justify-content-start">
+        <h3 class="subt">III. Gastos o Cargos por Productos o Servicios Voluntariamente Contratados</h3>
+    </div>
+    
+    <div class="container">
+        <hr>
+        <div>
+            <p>Valor de la Cuota de la Referencia: x</p>
+        </div>
+        <table class="table table-bordered w-auto ">
+            <tbody>
+                <tr>
+                    <th>Tipo Seguro</th>
+                    <th>Costo Mensual ($)</th>
+                    <th>Costo Total ($)</th>
+                    <th>Proveedor de Servicios</th> 
+                    <th>Cobertura</th>
+                </tr>
+                <tr>
+                    <th>x</th>
+                    <th>x</th>
+                    <th>x</th>
+                    <th>x</th>
+                    <th>x</th>
+                </tr>
+                <tr>
+                    <th>x</th>
+                    <th>x</th>
+                    <th>x</th>
+                    <th>x</th>
+                    <th>x</th>
+                </tr>
+                <tr>
+                    <th>x</th>
+                    <th>x</th>
+                    <th>x</th>
+                    <th>x</th>
+                    <th>x</th>
+                </tr>
+                <tr>
+                    <th>x</th>
+                    <th>x</th>
+                    <th>x</th>
+                    <th>x</th>
+                    <th>x</th>
+                </tr>
+                <tr>
+                    <th>Total:</th>
+                    <th>x</th>
+                    <th>x</th>
+                    <th>x</th>
+                    <th>x</th>
+                </tr>
+            </tbody>
+        </table>
+    
+
+
+    </div>
+    <div class="container justify-content-start">
+        <h3 class="subt">IV. Condiciones de Prepago</h3>
+    </div>
+    
+    <div class="container">
+        <hr>
+        <div class="row">
+            <div class="col">
+                <p>Cargo de Prepago:</p>
+            </div>
+            <div class="col">
+                <p>x</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>Plazo Aviso de Prepago:</p>
+            </div>
+            <div class="col">
+                <p>x</p>
+            </div>
+        </div>
+    </div>
+    <div class="container justify-content-start">
+        <h3 class="subt">V. Recargo por Atrasos</h3>
+    </div>
+    
+    <div class="container">
+        <hr>
+        <div class="row">
+            <div class="col">
+                <p>Interes Moratorio:</p>
+            </div>
+            <div class="col">
+                <p>Tasa Máxima Convencional</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <p>Gastos de Cobranza (%):</p>
+            </div>
+            <div class="col">
+                <p>Un 9% para las deudas o cuotas de hasta 10 UF</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col">
+                <p>Un 6% para las deudas de 10 y hasta 50 UF</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col">
+                <p>Un 3% para deudas de más de 50 UF</p>
+            </div>
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <p class="">El costo total de prepago no considera los recargos por atrasos en el pago del crédito</p>
+    </div>
+</body>
+</html>
